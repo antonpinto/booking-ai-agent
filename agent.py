854dea4aa9ec76b51417695abe96e7a2bcd5a51c
Antonio Pinto <antoniosilvapinto@gmail.com>
2024-10-30 15:27:13 +0000

    Fix agent

--- agent.py
+++ agent.py
@@ -4,21 +4,21 @@ from langgraph.prebuilt import create_react_agent
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 
-def office_occupation(location: str) -> str:
+def office_occupation(location: str, date: str) -> str:
     """Returns how many people are in the office"""
-    return f"Theres are 2 people in the {location} office"
+    return f"There will be 2 people in the {location} office on {date}"
 
 
-def book_room(location: str) -> str:
-    """Books a room in the office"""
-    return f"Room 1 booked at the {location} office for 2 people"
+def book_room(location: str, date: str) -> str:
+    """Books a room on [date] in the [location]"""
+    return f"Room 1 booked at the {location} office on {date} for 2 people"
 
 
 if __name__ == '__main__':
     load_dotenv()
 
-    # Book a room in the Lisbon office
-    # How many people are in the Lisbon office
+    # Book a room on 2024-10-31 in the Lisbon office
+    # How many people will be in the Lisbon office on 2024-10-31
     # bye
 
     tools = [book_room, office_occupation]
