07f7c200f5f6dc09ee1c2e6573029eb4e18058f4
Antonio Pinto <antoniosilvapinto@gmail.com>
2024-11-08 18:46:13 +0000

    API finalized

--- api.py
+++ api.py
@@ -8,44 +8,42 @@ class Booking(BaseModel):
     location: str
     date: str
     person: str
-    desk: str
+    desk: str = None
 
 
-app = FastAPI()
 desks = ["D001", "D002", "D003", "D004"]
 bookings = [
     Booking(
         location="Lisbon", date="2024-12-01", person="John Doe", desk=desks[0]
     )
 ]
+app = FastAPI()
 
 
 @app.post("/bookings/", response_model=Booking)
-def create_booking(location: str, date: str, person: str):
+def create_booking(booking: Booking):
     existing_reserve = [
-        b for b in bookings if b.date == date and b.person == person
+        b for b in bookings if b.date == booking.date and b.person == booking.person
     ]
     if len(existing_reserve) == 1:
         return existing_reserve[0]
 
-    reserved_desks = [b.desk for b in bookings if b.date == date]
+    reserved_desks = [b.desk for b in bookings if b.date == booking.date]
     free_desks = [d for d in desks if d not in reserved_desks]
 
     if len(free_desks) == 0:
         raise HTTPException(status_code=400, detail="No free desks!")
 
-    booking = Booking(
-        location=location, date=date, person=person, desk=free_desks[0]
-    )
+    booking.desk = free_desks[0]
     bookings.append(booking)
     return booking
 
 
 @app.get("/bookings/", response_model=List[Booking])
-def get_bookings(date: str = None, location: str = "LIS"):
+def get_bookings(date: str = None, location: str = "Lisbon"):
     return [b for b in bookings if (b.date == date or date is None) and b.location == location]
 
 
 @app.get("/requesters/", response_model=List[str])
-def get_booking_requesters(date: str = None, location: str = "LIS"):
-    return [b.person for b in bookings if (b.date == date or date is None) and b.location == location]
+def get_booking_requesters(date: str, location: str = "Lisbon"):
+    return [b.person for b in bookings if b.date == date and b.location == location]
--- chatbot.py
+++ chatbot.py
@@ -10,12 +10,10 @@ import requests
 @tool(return_direct=True)
 def book_desk(location: str, date: str) -> str:
     """Books a desk in the [location] on [date]"""
-    headers = {'Content-type': 'application/json'}
     data = {'location': location, 'date': date, 'person': 'Antonio Pinto'}
     print(data)
 
-    #    res = requests.post('http://localhost:8000/bookings', data=json.dumps(data), headers=headers)
-    res = requests.post(f'http://localhost:8000/bookings/?location={location}&date={date}&person=Antonio Pinto')
+    res = requests.post('http://localhost:8000/bookings', json=data)
     if res.status_code == 200:
         booking = res.json()
         return f"Desk {booking["desk"]} booked for you in the {booking["location"]} office on {booking["date"]}"
@@ -26,11 +24,6 @@ def book_desk(location: str, date: str) -> str:
 @tool(return_direct=True)
 def get_office_occupation(location: str, date: str) -> str:
     """Returns who will be in the [location] on [date]"""
-    headers = {'Content-type': 'application/json'}
-    data = {'location': location, 'date': date, 'person': 'Antonio Pinto'}
-    print(data)
-
-    #    res = requests.post('http://localhost:8000/bookings', data=json.dumps(data), headers=headers)
     res = requests.get(f'http://localhost:8000/requesters/?location={location}&date={date}')
     if res.status_code != 200:
         return 'An error occurred.'
@@ -40,7 +33,9 @@ def get_office_occupation(location: str, date: str) -> str:
         return 'An error occurred.'
 
     count = len(persons)
-    if count == 1:
+    if count == 0:
+        return f"No one will be there"
+    elif count == 1:
         return f"One person will be there: {persons[0]}"
 
     return f"{count} persons will be there: {', '.join(persons)}"
@@ -54,7 +49,7 @@ def get_weather(city: Literal["nyc", "sf", "lisbon"]):
     elif city == "sf":
         return "It's always sunny in sf"
     elif city == "lisbon":
-        return "It will be clear skies and warm"
+        return "It will be Sunny changing to partly cloudy by nighttime. Most likely maximum day time temperature 22 degrees Celsius and highest possible is 23 degrees"
     else:
         raise AssertionError("Unknown city")
 
@@ -64,7 +59,7 @@ tools = [get_weather, book_desk, get_office_occupation]
 model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
 graph = create_react_agent(model, tools=tools)
 
-st.title("💬 Chatbot")
+st.title("💬 Personal Assistant Bot")
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
@@ -90,7 +85,8 @@ if prompt := st.chat_input():
     # Queries:
     #
     # Book me a desk on 2024-12-01 in the Lisbon office
-    # Who will be there on 2024-12-01 in the Lisbon office
+    # Who will be in the Lisbon office on 2024-12-01
+    # And how will be the weather like in Lisbon on 2024-12-01
+
     # what's the weather in sf
     # what's the weather in nyc
-    # What will be the weather like in nyc on 2024-12-01
